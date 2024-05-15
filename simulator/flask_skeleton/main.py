fd8f0a57b03092c6fee3a672fdc8a0465b4b29ad
Katja Mathesius <katmathematics@gmail.com>
2024-05-14 20:32:15 -0500

    Simulation Hosting Overhaul
    Complete overhaul of the code in order to make it more friendly with being hosted

---
+++ simulator/flask_skeleton/main.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+
+app.secret_key = os.environ.get('FLASK_SECRET')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_DOMAIN'] = '.localhost'
+
+app.config.update(
+    TEMPLATES_AUTO_RELOAD = True
+)
+
+import routes
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+if __name__ == "__main__":
+  #    app = create_app()
+  print(" Starting app...")
+  app.run(host="0.0.0.0", port=5000)
